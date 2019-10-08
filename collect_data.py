782bc456a21b64192ea48f32e12d473c0794132f
dgxadmin <dgxadmin@KRDMAGM03P.mobis.com>
2019-10-08 05:04:35 +0000

    aux_info parser

--- collect_data.py
+++ collect_data.py
@@ -1,12 +1,44 @@
 from os import listdir
+from elasticsearch import Elasticsearch
 
-mypath = "/home/dgxadmin"
+mypath = "/ifs/mfc/data/mobis/L4_FR_CMR/10_raw"
 
 from os.path import isfile, join
 onlyfiles = [f for f in listdir(mypath)]
 
-for dd in onlyfiles:
+def getAuxInfo(filePath):
+    print(filePath)
+    file = open(filePath)
+    aa = [line.replace("\n","") for line in file.readlines()]
+    splittedItems = [item.split(": ") for item in aa]
+    file.close()
+    obj = {}
+    for splittedItem in splittedItems:
+        if len(splittedItem) > 1:
+            obj[splittedItem[0]] = splittedItem[1]
+    return obj
     
-    if(isfile(join(mypath, dd)) == False ):
-        print(dd)
+def getLogObjects(filePath):
+    print(filePath)
+    file = open(filePath)
+    print(file.read())
+    file.close()
+
+
+for dirName in onlyfiles:
+    filePath = join(mypath, dirName)
+    yyyymmdd = dirName.split("_")
+    yymmdd = yyyymmdd[2][2:]
+    print("yymmdd=>", yymmdd)
+    
+    if(isfile(filePath) == False ):
+        print(dirName, filePath)
+        auxInfo = getAuxInfo(filePath + "/aux_info")
+        print(auxInfo)
+
+        logObjPath = "{}/{}/{}_Data_Log_Object.csv".format(filePath, yymmdd, yymmdd)
+        #print(logObjPath)
+        #getLogObjects(logObjPath)
+
+
 
